9aeebcdb22da1cf7be263d8be00a8fcb8b130433
zohaib <zohaibshafiq2005@gmail.com>
2024-08-09 22:47:40 +0500

    community page

--- app/community/page.js
+++ app/community/page.js
@@ -1,48 +1,39 @@
+import Image from "next/image";
+
+import mealIcon from "@/assets/icons/meal.png";
+import communityIcon from "@/assets/icons/community.png";
+import eventsIcon from "@/assets/icons/events.png";
 import classes from "./page.module.css";
 
-export default function ShareMealPage() {
+export default function CommunityPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Share your <span className={classes.highlight}>favorite meal</span>
+          One shared passion: <span className={classes.highlight}>Food</span>
         </h1>
-        <p>Or any other meal you feel needs sharing!</p>
+        <p>Join our community and share your favorite recipes!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
-          <div className={classes.row}>
-            <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
-            </p>
-            <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
-            </p>
-          </div>
-          <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
-          </p>
-          <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
-          </p>
-          <p>
-            <label htmlFor="instructions">Instructions</label>
-            <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
-              required
-            ></textarea>
-          </p>
-          IMAGE PICKER
-          <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
-          </p>
-        </form>
+        <h2>Community Perks</h2>
+
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt="A delicious meal" />
+            <p>Share & discover recipes</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="A crowd of people, cooking" />
+            <p>Find new friends & like-minded people</p>
+          </li>
+          <li>
+            <Image
+              src={eventsIcon}
+              alt="A crowd of people at a cooking event"
+            />
+            <p>Participate in exclusive events</p>
+          </li>
+        </ul>
       </main>
     </>
   );
