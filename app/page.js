1474fce9236530d355fd6f202907e318f26266d4
zohaib <zohaibshafiq2005@gmail.com>
2024-08-09 13:05:46 +0500

    meals share & dynamic page

--- app/page.js
+++ app/page.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import classes from "./globals.css";
+import MainHeader from "@/components/main-header/main-header";
 
 export default function Home() {
   return (
-    <>
-      <header className={classes.header}>
-        <div className={classes.slideshow}></div>
-        <div>
-          <div className={classes.hero}></div>
-        </div>
-      </header>
-      <main></main>
-    </>
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Share Meal</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
+    </main>
   );
 }
