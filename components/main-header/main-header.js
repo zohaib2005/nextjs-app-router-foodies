f5d5a8322542bbd1f82a4d8076617c8286609c30
zohaib <zohaibshafiq2005@gmail.com>
2024-08-11 12:51:19 +0500

    browse meals

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   console.log("Executing../");
+
   return (
     <>
       <MainHeaderBackground />
@@ -18,10 +20,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
