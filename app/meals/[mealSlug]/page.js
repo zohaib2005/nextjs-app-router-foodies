822fc437be405a98309d5a1119af885e50bdc89f
zohaib <zohaibshafiq2005@gmail.com>
2024-08-12 11:44:38 +0500

    page not found

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,4 +1,23 @@
+import Image from "next/image";
+import classes from "./../../page.module.css";
+
 export default function MealDetailsPage() {
-  console.log("Meals page");
-  return <h1>Meal Details</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+          </p>
+        </div>
+      </header>
+      <main>
+        <p></p>
+      </main>
+    </>
+  );
 }
